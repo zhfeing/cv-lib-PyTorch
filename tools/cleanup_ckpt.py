9bd98060f94cacd6b112268b88e78ba01ad52daa
zhfeing <zhf666968@gmail.com>
2021-03-21 15:44:14 +0800

    fixed cleanup tool

--- tools/cleanup_ckpt.py
+++ tools/cleanup_ckpt.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import argparse
 
 
@@ -9,22 +8,22 @@ def rm_ckpts(ckpt_dir: str, test: bool):
             print("\nIn dir:", dirpath)
             filenames.sort()
             filenames.pop(0)
-            assert "best.pth" not in filenames
-            iters = list(int(a.split("-")[1][:-4]) for a in filenames)
-            iters.sort()
-            iters.pop(-1)
-            for it in iters:
-                fp = os.path.join(dirpath, "iter-{}.pth".format(it))
-                if test:
-                    print(fp, os.path.isfile(fp))
-                else:
-                    os.remove(fp)
+        assert "best.pth" not in filenames
+        iters = list(int(a.split("-")[1][:-4]) for a in filenames)
+        iters.sort()
+        iters.pop(-1)
+        for it in iters:
+            fp = os.path.join(dirpath, "iter-{}.pth".format(it))
+            if test:
+                print(fp, os.path.isfile(fp))
+            else:
+                os.remove(fp)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--root")
-    parser.add_argument("--test", type=int, default=1)
+    parser.add_argument("--test", action="store_true")
     args = parser.parse_args()
 
     root = os.path.expanduser(args.root)
