0193041a627726549f4df101140a7d0b9de725d6
zhfeing <zhf666968@gmail.com>
2021-10-31 23:41:01 +0800

    added sam optimizer

--- cv_lib/optimizers/__init__.py
+++ cv_lib/optimizers/__init__.py
@@ -1,11 +1,13 @@
 from typing import Dict, Any, List
 import copy
+from functools import partial
 
 from torch import nn
 from torch.optim import *
 
 from cv_lib.utils import to_json_str
 from cv_lib.utils import log_utils
+from .sam import SAM
 
 
 __REGISTERED_OPTIMIZERS__: Dict[str, Optimizer] = {
@@ -40,3 +42,28 @@ def get_optimizer(params: List[Dict[str, nn.Parameter]], optimizer_cfg: Dict[str
     logger.info("Using {} optimizer with config {}".format(opt_name, to_json_str(optim_cfg)))
     optimizer = __REGISTERED_OPTIMIZERS__[opt_name](params, **optim_cfg)
     return optimizer
+
+
+def get_sam_optimizer(params: List[Dict[str, nn.Parameter]], optimizer_cfg: Dict[str, Dict[str, Any]]) -> SAM:
+    logger = log_utils.get_master_logger("get_sam_optimizer")
+    optimizer_cfg = copy.deepcopy(optimizer_cfg)
+    base_cfg = optimizer_cfg["base"]
+    sam_cfg = optimizer_cfg["sam"]
+    base_name = base_cfg.pop("name")
+
+    logger.info("Using {} base optimizer with base config {}, sam config {}".format(
+        base_name,
+        to_json_str(sam_cfg),
+        to_json_str(sam_cfg),
+    ))
+    base_optimizer = partial(
+        __REGISTERED_OPTIMIZERS__[base_name],
+        **base_cfg
+    )
+    sam = SAM(
+        params,
+        base_optimizer=base_optimizer,
+        **sam_cfg
+    )
+    return sam
+
