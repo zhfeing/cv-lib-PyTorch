7f3753d09f81f33feb48654550004505a1c80f44
zhfeing <zhf666968@gmail.com>
2022-05-16 15:13:28 +0800

    support make_partial for cifar

--- cv_lib/classification/data/cifar.py
+++ cv_lib/classification/data/cifar.py
@@ -1,19 +1,37 @@
-from typing import Callable, Tuple, Optional, Dict, Any
+import collections
+from typing import Callable, Tuple, Optional, Dict, Any, List
 
 from PIL import Image
+import numpy as np
 
 import torch
 import torchvision.datasets
 from torchvision.datasets.utils import verify_str_arg
 
 from .classification_dataset import ClassificationDataset
-from cv_lib.utils import log_utils
+from cv_lib.utils import log_utils, random_pick_instances
 
 
 CIFAR_10_MEAN = [0.4914, 0.4822, 0.4465]
 CIFAR_10_STD = [0.2023, 0.1994, 0.2010]
 CIFAR_100_MEAN = [0.5071, 0.4867, 0.4408]
 CIFAR_100_STD = [0.2023, 0.1994, 0.2010]
+
+
+def _make_partial(data: np.ndarray, targets: List[int], percent: float) -> Tuple[np.ndarray, List[int]]:
+    bucket = collections.defaultdict(list)
+    for id, c in enumerate(targets):
+        bucket[c].append(data[id])
+
+    data: List[np.ndarray] = list()
+    targets: List[int] = list()
+    for c_id, k in enumerate(bucket):
+        instance = random_pick_instances(bucket[k], percent, seed=c_id)
+        target = list(k for _ in range(len(instance)))
+        data.append(instance)
+        targets.extend(target)
+    data = np.concatenate(data)
+    return data, targets
 
 
 class CIFAR_10(ClassificationDataset):
@@ -23,6 +41,7 @@ class CIFAR_10(ClassificationDataset):
         split: str = "train",
         resize: Optional[Tuple[int]] = None,
         augmentations: Callable[[Image.Image, Dict[str, Any]], Tuple[Image.Image, Dict[str, Any]]] = None,
+        make_partial: float = None
     ):
         """
         Args:
@@ -41,15 +60,20 @@ class CIFAR_10(ClassificationDataset):
             train=self.split == "train",
             download=False
         )
-        self._init_dataset()
+        self._init_dataset(make_partial)
 
-    def _init_dataset(self):
+    def _init_dataset(self, make_partial: float):
         self.CLASSES = self.cifar.classes
         for i, cls_name in enumerate(self.CLASSES):
             self.label_info[i] = cls_name
             self.label_map[cls_name] = i
         self.dataset_mean = CIFAR_10_MEAN
         self.dataset_std = CIFAR_10_STD
+        # make partial
+        if make_partial is not None:
+            data, targets = _make_partial(self.cifar.data, self.cifar.targets, make_partial)
+            self.cifar.data = data
+            self.cifar.targets = targets
 
     def __len__(self):
         return len(self.cifar)
@@ -71,6 +95,7 @@ class CIFAR_100(ClassificationDataset):
         split: str = "train",
         resize: Optional[Tuple[int]] = None,
         augmentations: Callable[[Image.Image, Dict[str, Any]], Tuple[Image.Image, Dict[str, Any]]] = None,
+        make_partial: float = None
     ):
         """
         Args:
@@ -89,15 +114,20 @@ class CIFAR_100(ClassificationDataset):
             train=self.split == "train",
             download=False
         )
-        self._init_dataset()
+        self._init_dataset(make_partial)
 
-    def _init_dataset(self):
+    def _init_dataset(self, make_partial: float):
         self.CLASSES = self.cifar.classes
         for i, cls_name in enumerate(self.CLASSES):
             self.label_info[i] = cls_name
             self.label_map[cls_name] = i
         self.dataset_mean = CIFAR_100_MEAN
         self.dataset_std = CIFAR_100_STD
+        # make partial
+        if make_partial is not None:
+            data, targets = _make_partial(self.cifar.data, self.cifar.targets, make_partial)
+            self.cifar.data = data
+            self.cifar.targets = targets
 
     def __len__(self):
         return len(self.cifar)
