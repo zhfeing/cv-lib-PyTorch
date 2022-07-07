dfcafb8b94c9fd0e12b868e879a19fe575ae9c66
zhfeing <zhf666968@gmail.com>
2022-07-07 10:29:53 +0800

    update alexnet

--- cv_lib/classification/models/alexnet.py
+++ cv_lib/classification/models/alexnet.py
@@ -30,7 +30,7 @@ class AlexNet(nn.Module):
 
     def __init__(self, num_classes: int = 1000) -> None:
         super(AlexNet, self).__init__()
-        self.features = nn.Sequential(
+        features = [
             Conv(3, 64, kernel_size=11, stride=4, padding=2),
             nn.MaxPool2d(kernel_size=3, stride=2),
             Conv(64, 192, kernel_size=5, padding=2),
@@ -38,14 +38,16 @@ class AlexNet(nn.Module):
             Conv(192, 384, kernel_size=3, padding=1),
             Conv(384, 256, kernel_size=3, padding=1),
             Conv(256, 256, kernel_size=3, padding=1),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        ]
+        self.features = nn.ModuleList(features)
         self.gap = nn.AdaptiveAvgPool2d(1)
         self.flatten = nn.Flatten(1)
         self.fc = nn.Linear(256, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.features(x)
+        for layer in self.features:
+            x = layer(x)
         x = self.gap(x)
         x = self.flatten(x)
         x = self.fc(x)
